e6f36131e94e1d98629a46e5f7647141bc817960
MohamedAliBousba <im_bousba@esi.dz>
2021-11-28 08:53:25 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GoogleLogin from "react-google-login";
+import FacebookLogin from 'react-facebook-login';
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+
+  const login = (res: any) => {
+    console.log(res);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID as string}
+        buttonText="Login"
+        onSuccess={login}
+        onFailure={login}
+        cookiePolicy={"single_host_origin"}
+      />
+
+      <div style={{ margin: "0 10px" }} />
+
+      <FacebookLogin
+        appId={process.env.REACT_APP_FB_APP_ID as string}
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={login}
+        callback={login}
+      />
     </div>
   );
-}
+};
 
 export default App;
